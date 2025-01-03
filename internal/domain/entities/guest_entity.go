98e00b5be5e862e1ddc0fcca96e0ac69cfca1017
Andhika-GIT <andhikapr081@gmail.com>
2025-01-03 16:30:39 +0700

    refactor: add json toall entities that doesn't have json before

--- internal/domain/entities/guest_entity.go
+++ internal/domain/entities/guest_entity.go
@@ -3,13 +3,13 @@ package entities
 import "time"
 
 type Guest struct {
-	ID          int64     `gorm:"primary_key;column:id"`
-	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
-	FullName    string    `gorm:"column:full_name"`
-	Email       string    `gorm:"column:email"`
-	NationalID  string    `gorm:"column:national_id"`
-	Nationality string    `gorm:"column:nationality"`
-	CountryFlag string    `gorm:"column:country_flag"`
+	ID          int64     `json:"id" gorm:"primary_key;column:id"`
+	CreatedAt   time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime"`
+	FullName    string    `json:"full_name" gorm:"column:full_name"`
+	Email       string    `json:"email" gorm:"column:email"`
+	NationalID  string    `json:"national_id" gorm:"column:national_id"`
+	Nationality string    `json:"nationality" gorm:"column:nationality"`
+	CountryFlag string    `json:"country_flag" gorm:"column:country_flag"`
 }
 
 func (g *Guest) TableName() string {
