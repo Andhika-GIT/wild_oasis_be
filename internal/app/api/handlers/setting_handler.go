2d0198e16d66a864448b3f584fd660f0ee211f38
Andhika-GIT <andhikapr081@gmail.com>
2025-01-04 16:40:23 +0700

    feat: new service, repo & hander for setting

---
+++ internal/app/api/handlers/setting_handler.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Andhika-GIT/wild_oasis_be/internal/app/services"
+	"github.com/Andhika-GIT/wild_oasis_be/internal/app/web"
+	utils "github.com/Andhika-GIT/wild_oasis_be/pkg/web"
+)
+
+type SettingHandler struct {
+	service *services.SettingService
+}
+
+func NewSettingHandler(service *services.SettingService) *SettingHandler {
+	return &SettingHandler{
+		service: service,
+	}
+}
+
+func (c *SettingHandler) GetSetting(w http.ResponseWriter, r *http.Request) {
+	settingResponse, err := c.service.GetSetting(r.Context())
+
+	if err != nil {
+		utils.SendResponse(w, http.StatusBadRequest, web.ErrorResponse{
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	utils.SendResponse(w, http.StatusOK, web.Response{
+		Code:    http.StatusOK,
+		Message: "Sucessfully get setting",
+		Data:    settingResponse,
+	})
+}
