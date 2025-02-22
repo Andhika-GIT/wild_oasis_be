da30c0a0361c03a2b29d52265fe0c186c8bcf8f1
Andhika-GIT <andhikapr081@gmail.com>
2025-02-22 20:59:29 +0700

    refactor: combine read json and send response function into one file

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+func ReadBodyRequest(request *http.Request, requestData interface{}) error {
+	decoder := json.NewDecoder(request.Body)
+
+	err := decoder.Decode(requestData)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func SendResponse(w http.ResponseWriter, statusCode int, response interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
